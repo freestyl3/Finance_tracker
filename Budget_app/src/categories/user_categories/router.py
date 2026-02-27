2932826f733af51ab1223972dc896b849e265927
Alexey Tyaguschev <lehatop450tav@gmail.com>
2026-02-27 10:58:42 +0300

    added user categories router

---
+++ Budget_app/src/categories/user_categories/router.py
@@ -0,0 +1,43 @@
+import uuid
+
+from fastapi import APIRouter
+
+from src.categories.base.schemas import (
+    CategoryCreate, CategoryRead, CategoryUpdate
+)
+from src.categories.user_categories.dependecies import UserCategoryServiceDep
+from src.auth.dependencies import CurrentUser
+
+router = APIRouter()
+
+@router.post("/", response_model=CategoryRead)
+async def create_user_category(
+    category: CategoryCreate,
+    service: UserCategoryServiceDep,
+    current_user: CurrentUser
+):
+    return await service.create(category, current_user.id)
+
+@router.get("/", response_model=list[CategoryRead])
+async def get_all_user_categories(
+    service: UserCategoryServiceDep,
+    current_user: CurrentUser
+):
+    return await service.get_all(current_user.id)
+
+@router.put("/{category_id}", response_model=CategoryRead)
+async def update_user_category(
+    category_id: uuid.UUID,
+    category_update: CategoryUpdate,
+    service: UserCategoryServiceDep,
+    current_user: CurrentUser
+):
+    return await service.update(category_id, category_update, current_user.id)
+
+@router.delete("/{category_id}")
+async def soft_delete_user_category(
+    category_id: uuid.UUID,
+    service: UserCategoryServiceDep,
+    current_user: CurrentUser
+):
+    return await service.soft_delete(category_id, current_user.id)
