deac9ea560a90b1f1aa67ebef042f11f38e8824d
Alexey Tyaguschev <lehatop450tav@gmail.com>
2026-02-27 10:47:51 +0300

    added system categories router

---
+++ Budget_app/src/categories/system_categories/router.py
@@ -0,0 +1,37 @@
+import uuid
+
+from fastapi import APIRouter
+
+from src.categories.base.schemas import CategoryRead, CategoryCreate
+from src.categories.system_categories.schemas import SystemCategoryUpdate
+from src.categories.system_categories.dependecies import SystemCategoryServiceDep
+
+router = APIRouter()
+
+## ВСЕ ЗАЩИТИТЬ ПРАВАМИ АДМИНА
+
+@router.post("/", response_model=CategoryRead)
+async def create_system_category(
+    category: CategoryCreate,
+    service: SystemCategoryServiceDep
+):
+    return await service.create(category)
+
+@router.get("/", response_model=list[CategoryRead])
+async def get_all_system_categories(service: SystemCategoryServiceDep):
+    return await service.get_all()
+
+@router.put("/{category_id}", response_model=CategoryRead)
+async def update_system_category(
+    category_id: uuid.UUID,
+    category_update: SystemCategoryUpdate,
+    service: SystemCategoryServiceDep
+):
+    return await service.update(category_id, category_update)
+
+@router.delete("/{category_id}")
+async def delete_system_category(
+    category_id: uuid.UUID,
+    service: SystemCategoryServiceDep
+):
+    return await service.delete(category_id)
