6e363fb808c8a6f1d1d2e905b4ea821541386801
freestyl3 <lehatop450tav@gmail.com>
2026-03-06 23:20:43 +0300

    Added operation repository and service dependencies and renamed get_account_repository and get_account_service methods

--- Budget_app/src/accounts/dependencies.py
+++ Budget_app/src/accounts/dependencies.py
@@ -7,14 +7,14 @@ from src.database.db_helper import db_helper
 from src.accounts.repository import AccountRepository
 from src.accounts.service import AccountService
 
-async def get_accounts_repository(
+async def get_account_repository(
         session: AsyncSession = Depends(db_helper.session_dependency)
 ) -> AccountRepository:
     return AccountRepository(session)
 
-async def get_accounts_service(
-        repo: AccountRepository = Depends(get_accounts_repository)
+async def get_account_service(
+        repo: AccountRepository = Depends(get_account_repository)
 ) -> AccountService:
     return AccountService(repo)
 
-AccountServiceDep = Annotated[AccountService, Depends(get_accounts_service)]
+AccountServiceDep = Annotated[AccountService, Depends(get_account_service)]
